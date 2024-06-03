b8e189971ad45a08e8dab7cdafb9877f1044fc2b
binilsen <binil.senb@techversantinfo.com>
2024-06-03 10:40:14 +0530

    Fix user login spec

--- spec/support/shared/request_helper.rb
+++ spec/support/shared/request_helper.rb
@@ -7,7 +7,7 @@ module Requests
 
   def generate_token(user)
     payload = { account_id: user.id, authenticated_by: ['password'] }
-    JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
+    JWT.encode payload, 'test', 'HS256'
   end
 
   module JsonHelpers
